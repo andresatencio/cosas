7782ab3f49d51c8330d31ed46483123ed310de00
Andres Atencio <and.atencio@gmail.com>
2014-11-18 14:24:24 -0300

    cts

--- agregarLayerKML.js
+++ agregarLayerKML.js
@@ -1,6 +1,6 @@
  // https://raw.githubusercontent.com/andresatencio/cosas/master/San%20Nicolas.kml
   // https://github.com/andresatencio/cosas/raw/master/Obra%20alimentador%20Moreno.kmz
-  var map = 'Objeto mapa de google'
+  // var map = 'Objeto mapa de google'
   var ctaLayer = new google.maps.KmlLayer({
     url: 'https://github.com/andresatencio/cosas/raw/master/Obra%20alimentador%20Moreno.kmz'
   });

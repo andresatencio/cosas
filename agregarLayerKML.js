582810ee92cb85425a42d0a8ffe9f478a7173b3a
Andres Atencio <and.atencio@gmail.com>
2014-11-26 10:04:17 -0300

    azul

--- agregarLayerKML.js
+++ agregarLayerKML.js
@@ -2,7 +2,7 @@
   // https://github.com/andresatencio/cosas/raw/master/Obra%20alimentador%20Moreno.kmz
   // var map = 'Objeto mapa de google'
   var ctaLayer = new google.maps.KmlLayer({
-    url: 'https://github.com/andresatencio/cosas/raw/master/Obra%20alimentador%20Moreno.kmz'
+    url: 'https://github.com/andresatencio/cosas/raw/master/Lincoln%20%2B%20Gral%20Villegas%20%2B%20Gral%20Pinto.kmz'
   });
 
   ctaLayer.setMap(map)
@@ -10,3 +10,32 @@ map.setCenter(new google.maps.LatLng(-34.332321,-60.211002))
 map.setCenter()
 map.setZoom()
 
+https://github.com/andresatencio/cosas/raw/master/Lincoln%20%2B%20Gral%20Villegas%20%2B%20Gral%20Pinto.kmz
+
+// Descargar consola
+(function(console){
+
+console.save = function(data, filename){
+
+    if(!data) {
+        console.error('Console.save: No data')
+        return;
+    }
+
+    if(!filename) filename = 'console.json'
+
+    if(typeof data === "object"){
+        data = JSON.stringify(data, undefined, 4)
+    }
+
+    var blob = new Blob([data], {type: 'text/json'}),
+        e    = document.createEvent('MouseEvents'),
+        a    = document.createElement('a')
+
+    a.download = filename
+    a.href = window.URL.createObjectURL(blob)
+    a.dataset.downloadurl =  ['text/json', a.download, a.href].join(':')
+    e.initMouseEvent('click', true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null)
+    a.dispatchEvent(e)
+ }
+})(console)
